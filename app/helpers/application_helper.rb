545d6f93d0d3c631e77e26506e18832c19bd0073
kuritaeiji <eiji199825@yahoo.co.jp>
2020-06-22 23:48:24 +0900

    full_title helperの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def full_title(yielded_title)
+    if yielded_title.empty?
+      'rails turorial'
+    else
+      "#{yielded_title} | rails tutorial"
+    end
+  end
 end
