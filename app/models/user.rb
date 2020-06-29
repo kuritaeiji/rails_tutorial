61cd06ab3a05bcf13687850995d330bd6ebc8284
kuritaeiji <eiji199825@yahoo.co.jp>
2020-06-30 08:44:04 +0900

    パスワードの再設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save { self.email = email.downcase }
   before_create :create_activation_digest
 
@@ -24,10 +24,10 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, digest)
   end
 
-  def authenticated?(attribute, remember_token)
-    digest = self.send("#{attribute_digest}_digest")
+  def authenticated?(attribute, token)
+    digest = self.send("#{attribute}_digest")
     return false if remember_digest.nil?
-    remember_token == digest
+    token == digest
   end
 
   def forget
@@ -36,6 +36,20 @@ class User < ApplicationRecord
 
   def activate
     update_attributes(activated: true, activated_at: Time.zone.now)
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired
+    reset_sent_at < 2.hours.ago
   end
 
   private
